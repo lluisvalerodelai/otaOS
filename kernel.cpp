0dc029893949e3ebdb7bdc8be949e1a2165572a6
Luis Valero <lluisvalerodelai@gmail.com>
2025-02-16 15:47:52 +0100

    first commit

---
+++ kernel.cpp
@@ -0,0 +1,15 @@
+
+void printf(char *str) {
+	unsigned short* VideoMemory = (unsigned short*)0xb8000;
+
+	for (int i = 0; i < '\0'; ++i) {
+		VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+	}
+}
+
+extern "C" void kernelMain() {
+	printf("hello world!");
+  while (1) {
+  }; // we need to kernel to just stop, otherwise the cpu will just keep running
+     // insn that are undefined and we get undefined behaviour
+}
